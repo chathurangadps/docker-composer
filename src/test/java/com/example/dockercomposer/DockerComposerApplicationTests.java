5ed490409db5731388755c7fc0ae32b05a2f768c
chathurangadps <chathurangadps@gmail.com>
2021-10-13 23:21:25 +0530

    first commit

---
+++ src/test/java/com/example/dockercomposer/DockerComposerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockercomposer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerComposerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
